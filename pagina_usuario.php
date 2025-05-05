303f7607e4af2e8fff6b73625bf43a07e90ef7f3
kromero14 <keinerromero@colegioguanenta.edu.co>
2025-05-05 18:25:56 -0500

    se desarrollo el repositorio

---
+++ pagina_usuario.php
@@ -0,0 +1,28 @@
+<?php
+    require'modelo/conexion.php';
+
+    session_start();
+
+    if(isset($_SESSION['username']))
+    {
+        $nombre_usuario = $_SESSION['username'];
+    }
+?>
+
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página del usuario</title>
+</head>
+<body>
+    <h1>Página del usuario</h1>
+    <hr>
+    <?php
+       echo 'Usuario: '.$nombre_usuario;
+    ?>
+    <hr>
+    <a href="modelo/cerrar_sesion.php">Cerrar Sesión</a>
+<body>
+</html>
